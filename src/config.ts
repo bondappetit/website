feeeec6f2ca7a1572f99911c1b90246457e9394a
lorof <gilyazov@list.ru>
2020-10-27 19:42:33 +0500

    add config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
-const config = {};
-
-export default config;
+export const config = {
+	POLLING_INTERVAL: 12000,
+	RPC_URL: '127.0.0.1:8545',
+	CHAIN_IDS: [1, 3, 4, 5, 42, 999]
+};
--- src/wallets/common/connectors.ts
+++ src/wallets/common/connectors.ts
@@ -2,19 +2,16 @@ import type { AbstractConnector } from '@web3-react/abstract-connector';
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { LedgerConnector } from '@web3-react/ledger-connector';
 
-// TODO: move to config
-const POLLING_INTERVAL = 12000;
-const RPC_URL = '127.0.0.1:8545';
-const CHAIN_IDS = [1, 3, 4, 5, 42, 999];
+import { config } from 'src/config';
 
 export const injected = new InjectedConnector({
-	supportedChainIds: CHAIN_IDS
+	supportedChainIds: config.CHAIN_IDS
 });
 
 export const ledger = new LedgerConnector({
-	chainId: CHAIN_IDS[0],
-	url: RPC_URL,
-	pollingInterval: POLLING_INTERVAL
+	chainId: config.CHAIN_IDS[0],
+	url: config.RPC_URL,
+	pollingInterval: config.POLLING_INTERVAL
 });
 
 enum ConnectorNames {
