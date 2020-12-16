343c995a7a19e1b5af221da66edede1106709e12
lorof <gilyazov@list.ru>
2020-12-16 17:41:01 +0500

    fixed ucheck balance on split

--- src/profit-splitter/profit-splitter-deposit/use-split.ts
+++ src/profit-splitter/profit-splitter-deposit/use-split.ts
@@ -1,28 +1,20 @@
-import { useCallback, useRef } from 'react';
+import { useCallback } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import Web3 from 'web3';
 
 import { useProfitSplitterContract } from 'src/common';
 
 export const useSplit = (balance?: string) => {
-  const balanceRef = useRef(balance);
   const { account } = useWeb3React<Web3>();
 
   const profitSplitterContract = useProfitSplitterContract();
 
   const handleSplit = useCallback(async () => {
-    if (
-      !profitSplitterContract ||
-      !account ||
-      !balanceRef.current ||
-      Number(balanceRef.current) > 0
-    )
+    if (!profitSplitterContract || !account || !balance || Number(balance) > 0)
       return;
 
-    await profitSplitterContract.methods
-      .split(balanceRef.current)
-      .send({ from: account });
-  }, [profitSplitterContract, account]);
+    await profitSplitterContract.methods.split(balance).send({ from: account });
+  }, [profitSplitterContract, account, balance]);
 
   return handleSplit;
 };
