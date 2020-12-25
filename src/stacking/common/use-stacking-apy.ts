6eaafe70748b96d2c82de559dfa422ad5445b6de
lorof <gilyazov@list.ru>
2020-12-25 20:52:54 +0500

    fix stacking

--- src/stacking/common/use-stacking-apy.ts
+++ src/stacking/common/use-stacking-apy.ts
@@ -1,7 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
 import BN from 'bignumber.js';
-import { useWeb3React } from '@web3-react/core';
-import Web3 from 'web3';
 
 import { useNetworkConfig, useUniswapRouter } from 'src/common';
 import { StackingToken } from './use-stacking-balances';
@@ -16,14 +14,11 @@ export const useStackingApy = (balances: StackingToken[]) => {
   const uniswapRouter = useUniswapRouter();
   const networkConfig = useNetworkConfig();
   const [APY, setAPY] = useState<APYWithTokenName[]>([]);
-  const { account } = useWeb3React<Web3>();
 
   const handleGetTokenPrice = useCallback(async () => {
     const amountInBond = new BN(10)
       .pow(networkConfig.assets.Bond.decimals)
       .toString(10);
-
-    if (!account) return;
 
     const [
       ,
@@ -38,8 +33,8 @@ export const useStackingApy = (balances: StackingToken[]) => {
     const result = balances.map((balance) => ({
       ...balance,
       APY: new BN(balance.delta)
-        .multipliedBy(BLOCK_PER_YEAR)
         .multipliedBy(bondInUSDC)
+        .multipliedBy(BLOCK_PER_YEAR)
         .div(
           new BN(10)
             .pow(networkConfig.assets.Bond.decimals)
@@ -50,7 +45,7 @@ export const useStackingApy = (balances: StackingToken[]) => {
     }));
 
     if (result.length) setAPY(result);
-  }, [networkConfig, uniswapRouter, balances, account]);
+  }, [networkConfig, uniswapRouter, balances]);
 
   useEffect(() => {
     handleGetTokenPrice();
