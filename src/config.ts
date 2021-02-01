fd430ffee25fee1f577257f574fddc55eccd060e
Vladislav Komissarov <mastyf@gmail.com>
2021-02-01 11:26:05 +0300

    countdown update

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const config = {
   IS_DEV: process.env.NODE_ENV === 'development',
   DEFAULT_CHAIN_ID: process.env.REACT_APP_DEFAULT_CHAIN_ID,
   DEFAULT_NETWORK_CONFIG: defaultNetworkConfig,
-  COUNTDOWN_DATE: '2021-02-1',
+  COUNTDOWN_DATE: '2021-02-8',
   PORTIS_ID: process.env.REACT_APP_PORTIS_ID,
   FORTMATIC_KEY: process.env.REACT_APP_FORTMATIC_KEY,
   IS_INVEST: process.env.REACT_APP_IS_INVEST === 'true',
