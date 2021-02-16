e6a584675fcd74d7d59f8a0b3bfd41bd1148b8d1
Vladislav Komissarov <mastyf@gmail.com>
2021-02-16 18:41:47 +0300

    investor-deck -> litepaper

--- src/whitepaper/whitepaper.tsx
+++ src/whitepaper/whitepaper.tsx
@@ -52,7 +52,7 @@ export const WhitePaper: React.FC = () => {
               target="_blank"
               color="blue"
             >
-              ↓ investment-deck.pdf
+              ↓ litepaper.pdf
             </Link>
           </Typography>
         </div>
