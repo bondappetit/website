f15e69cff398bf2e33cf5f168da2d86120629b0e
lorof <gilyazov@list.ru>
2021-03-17 14:58:42 +0500

    add icon for each coin on governance calc

--- src/common/form-modal/form-modal-select.tsx
+++ src/common/form-modal/form-modal-select.tsx
@@ -5,6 +5,7 @@ import { ButtonBase } from '../button-base';
 import { useFormModalStyles } from './form-modal.styles';
 import { Asset } from '../types';
 import { humanizeNumeral } from '../bignumber';
+import { COIN_ICONS } from '../constants';
 
 export type FormModalSelectProps = {
   onChange: (value: string, balance: string) => void;
@@ -17,19 +18,26 @@ export const FormModalSelect: React.FC<FormModalSelectProps> = (props) => {
 
   return (
     <div className={classes.select}>
-      {props.options.map((option) => (
-        <ButtonBase
-          type="button"
-          key={option.symbol}
-          className={clsx(classes.selectOption, {
-            [classes.selectOptionActive]: option.symbol === props.value
-          })}
-          onClick={() => props.onChange(option.symbol, option.balance)}
-        >
-          <span>{option.symbol}</span>
-          <span>{humanizeNumeral(option.balance)}</span>
-        </ButtonBase>
-      ))}
+      {props.options.map((option) => {
+        const Icon = COIN_ICONS.get(option.symbol);
+
+        return (
+          <ButtonBase
+            type="button"
+            key={option.symbol}
+            className={clsx(classes.selectOption, {
+              [classes.selectOptionActive]: option.symbol === props.value
+            })}
+            onClick={() => props.onChange(option.symbol, option.balance)}
+          >
+            {Icon && <Icon />}
+            <span className={classes.currency}>{option.symbol}</span>
+            <span className={clsx(classes.currency, classes.selectBalance)}>
+              {humanizeNumeral(option.balance)}
+            </span>
+          </ButtonBase>
+        );
+      })}
     </div>
   );
 };
