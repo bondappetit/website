38c8fdf1f346a1dc002c535285de132a385b15e9
Vladislav Komissarov <mastyf@gmail.com>
2021-03-26 15:51:37 +0300

    mixBytes link

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -86,7 +86,7 @@ export const Main: React.FC = () => {
           <MainCollateral className={classes.section} />
           <MainAudit
             className={clsx(classes.section, classes.audit)}
-            auditLink="https://github.com/mixbytes/audits_public/tree/master/BondAppetit"
+            auditLink="https://github.com/mixbytes/audits_public/tree/a8edc9bf822cdc53c403fa18bd947e25bf5b2e0e/BondAppetit"
             companyLogo={<MixBytesLogo />}
           />
           <MainSteps className={classes.steps} />
