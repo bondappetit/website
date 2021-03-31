4001972468d68bca00564f700a1311fb30edc26e
Vladislav Komissarov <mastyf@gmail.com>
2021-03-31 10:00:40 +0300

    remove countdown

--- src/investing/common/investing-announcement/investing-announcement.tsx
+++ src/investing/common/investing-announcement/investing-announcement.tsx
@@ -13,21 +13,11 @@ export type InvestingAnnouncementProps = {
 export const InvestingAnnouncement: React.FC<InvestingAnnouncementProps> = (
   props
 ) => {
-  const [countdown, setCountdown] = React.useState(
-    dateUtils.countdown(config.COUNTDOWN_DATE)
-  );
   const classes = useInvestingAnnouncementStyles();
-
-  useTimeoutInterval(() => {
-    setCountdown(dateUtils.countdown(config.COUNTDOWN_DATE));
-  }, 1000);
 
   return (
     <div className={clsx(classes.announcement, props.className)}>
-      <Typography variant="h4" align="center" className={classes.title}>
-        The protocol starts in {countdown}
-      </Typography>
-      <Button onClick={props.onClick}>Notify me</Button>
+      <Button onClick={props.onClick}>Notify me as protocol starts</Button>
     </div>
   );
 };
