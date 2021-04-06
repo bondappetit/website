e866814b98ac43641e3b694957419348f0346dea
lorof <gilyazov@list.ru>
2021-04-06 16:00:27 +0500

    fix batch request typeing

--- src/common/use-batch-request.ts
+++ src/common/use-batch-request.ts
@@ -1,41 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+import { Extension } from 'web3-core';
 import { useCallback } from 'react';
+import Web3 from 'web3';
 
+import { NonPayableTx } from 'src/generate/types';
 import { useLibrary } from './use-library';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const isCallable = (
   call: unknown
-): call is { request: (...args: unknown[]) => Promise<void> } => {
-  return typeof call === 'object' && call !== null && 'request' in call;
+): call is {
+  request: (...args: unknown[]) => Extension['methods'][number];
+} => {
+  return typeof call === 'function' && call !== null && 'request' in call;
+};
+
+type Batch<
+  T extends (tx?: NonPayableTx | undefined) => Promise<any>
+> = T extends (tx?: NonPayableTx | undefined) => Promise<infer R> ? R : T;
+
+const makeBatchRequest = (library: Web3) => <T>(
+  calls: readonly (T | PromiseLike<T>)[],
+  callFrom?: string | null
+): Promise<Batch<T>[]> => {
+  const batch = new library.BatchRequest();
+
+  const promises = calls.map((call) => {
+    if (!isCallable(call)) return Promise.resolve(call);
+
+    return new Promise((resolve, reject) => {
+      const request = call.request(
+        { from: callFrom },
+        (error: Error, data: any) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        }
+      );
+      batch.add(request);
+    });
+  });
+
+  batch.execute();
+
+  return (Promise.all(promises) as unknown) as Promise<Batch<T>[]>;
 };
 
 export const useBatchRequest = () => {
   const library = useLibrary();
 
-  return useCallback(
-    function makeBatchRequest<T>(calls: T[], callFrom?: string | null) {
-      const batch = new library.BatchRequest();
-
-      const promises = calls.map((call) => {
-        return new Promise((resolve, reject) => {
-          const request = call.request({ from: callFrom }, (error, data) => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve(data);
-            }
-          });
-          batch.add(request);
-        });
-      });
-
-      batch.execute();
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return Promise.all(promises as any[]);
-    },
-    [library]
-  );
+  return useCallback(makeBatchRequest(library), [library]);
 };
