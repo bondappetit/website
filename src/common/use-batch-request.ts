b8116a49f3ec3e0f0236fef7ad1af0afb3a13d58
lorof <gilyazov@list.ru>
2021-04-14 22:11:40 +0500

    fix

--- src/common/use-batch-request.ts
+++ src/common/use-batch-request.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Extension } from 'web3-core';
 import { useCallback } from 'react';
 import Web3 from 'web3';
@@ -9,6 +8,7 @@ import { useLibrary } from './use-library';
 const isCallable = (
   call: unknown
 ): call is {
+  (): Promise<unknown>;
   request: (...args: unknown[]) => Extension['methods'][number];
 } => {
   return typeof call === 'function' && call !== null && 'request' in call;
@@ -19,11 +19,17 @@ type Batch<T> = T extends (tx?: NonPayableTx | undefined) => Promise<infer R>
   : T;
 
 const makeBatchRequest = (library: Web3) => <
-  T extends ((tx?: NonPayableTx | undefined) => Promise<any>) | any
+  T extends ((tx?: NonPayableTx | undefined) => Promise<unknown>) | unknown
 >(
   calls: T[],
   callFrom?: string | null
 ): Promise<Batch<T>[]> => {
+  if (window.ethereum?.isTrust) {
+    return Promise.all(
+      calls.map((call) => (isCallable(call) ? call() : Promise.resolve(call)))
+    ) as Promise<Batch<T>[]>;
+  }
+
   const batch = new library.BatchRequest();
 
   const promises = calls.map((call) => {
@@ -32,7 +38,7 @@ const makeBatchRequest = (library: Web3) => <
     return new Promise((resolve, reject) => {
       const request = call.request(
         { from: callFrom },
-        (error: Error, data: any) => {
+        (error: Error, data: unknown) => {
           if (error) {
             reject(error);
           } else {
