3834c37efe5023224d5987bad93c5651f3546ace
lorof <gilyazov@list.ru>
2021-04-22 20:31:18 +0500

    add change network metamask

--- src/binance/binance.tsx
+++ src/binance/binance.tsx
@@ -1,15 +1,20 @@
 import { useWeb3React } from '@web3-react/core';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { PageWrapper } from 'src/common';
 import { MainLayout } from 'src/layouts';
 import { BinanceChain } from './binance-chain';
 import { EthChain } from './eth-chain';
+import { setupNetwork } from './setup-binance';
 
 export type BinanceProps = unknown;
 
 export const Binance: React.VFC<BinanceProps> = () => {
   const { chainId } = useWeb3React();
+
+  useEffect(() => {
+    setupNetwork();
+  }, []);
 
   return (
     <MainLayout>
