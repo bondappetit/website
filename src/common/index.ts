639eff58994012bf8282723e8d8e5ead8049c579
artur <amamedbekov@tokenomica.com>
2021-04-29 17:12:08 +0300

    staking reward history

--- src/common/index.ts
+++ src/common/index.ts
@@ -66,3 +66,4 @@ export * from './add-token-metamask';
 export * from './use-approve';
 export * from './use-batch-request';
 export * from './use-interval-if-has-account';
+export * from './use-staking-reward-history';
---
+++ src/common/use-staking-reward-history.ts
@@ -0,0 +1,40 @@
+import { useLazyQuery } from './use-query';
+
+const url = 'http://localhost:8080/api';
+
+const QUERY = `
+  query($addresses:[AddressType!]!) {
+    stakingList(
+      filter:{address:$addresses}
+    ) {
+      rewardForDurationFloat
+      earnedFloat
+      rewardHistory {
+        totalReward
+        totalEarned
+      }
+    }
+  }
+`;
+
+export type StakingRewardHistory = {
+  totalReward: string;
+  totalEarned: string;
+};
+
+export type StakingReward = {
+  rewardForDurationFloat: string;
+  earnedFloat: string;
+  rewardHistory: StakingRewardHistory[];
+};
+
+export type StakingRewardPayload = {
+  data: {
+    stakingList: StakingReward[];
+  };
+};
+
+export const useStakingRewardHistory = () =>
+  useLazyQuery<StakingRewardPayload>(url, {
+    query: QUERY
+  });
