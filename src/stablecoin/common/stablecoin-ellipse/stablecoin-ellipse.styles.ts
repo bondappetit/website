41b55a79f5a4bfa2a74fb5d5d7c487db87d2b44c
lorof <gilyazov@list.ru>
2021-05-12 16:19:15 +0500

    wip: stable

--- src/stablecoin/common/stablecoin-ellipse/stablecoin-ellipse.styles.ts
+++ src/stablecoin/common/stablecoin-ellipse/stablecoin-ellipse.styles.ts
@@ -4,63 +4,34 @@ import { Theme } from 'src/common';
 
 export const useStablecoinEllipseStyles = createUseStyles(
   (theme: Theme) => ({
-    ellipse: {
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundSize: '85%',
-      marginBottom: 77,
-
-      [theme.breakpoints.lg()]: {
-        backgroundSize: '105%',
-        backgroundImage: `url(${theme.images.ellipseDesktop})`,
-        minHeight: 543,
-        paddingTop: 94
-      },
-
-      [theme.breakpoints.up(1600)]: {
-        backgroundSize: '85%'
-      }
-    },
-
-    title: {
-      marginBottom: 32,
-      maxWidth: 1200,
-      margin: '0 auto',
-
-      [theme.breakpoints.lg()]: {
-        fontSize: 64,
-        lineHeight: '72px'
-      }
+    root: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 416px',
+      gridGap: 40
     },
 
     info: {
-      marginBottom: 48,
+      '& *:first-child': {
+        marginBottom: 8
+      }
+    },
+
+    sellingBuying: {
       display: 'flex',
       flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-
-      [theme.breakpoints.md()]: {
-        flexDirection: 'row'
-      }
+      padding: '40px 63px 64px',
+      minHeight: 400
     },
 
     actions: {
-      textAlign: 'center',
+      width: '100%',
+      maxWidth: 240,
+      margin: 'auto auto 0',
+      display: 'flex',
+      flexDirection: 'column',
 
-      '& *:first-child': {
-        marginRight: 12
-      }
-    },
-
-    subtext: {
-      maxWidth: 960,
-      margin: 'auto'
-    },
-
-    supply: {
-      [theme.breakpoints.md()]: {
-        marginRight: 32
+      '& button:first-child': {
+        marginBottom: 16
       }
     },
 
--- src/stablecoin/common/stablecoin-ellipse/stablecoin-ellipse.tsx
+++ src/stablecoin/common/stablecoin-ellipse/stablecoin-ellipse.tsx
@@ -1,8 +1,7 @@
+import clsx from 'clsx';
 import React, { useMemo } from 'react';
-import { Link as ReactRouterLink } from 'react-router-dom';
 
-import { Typography, Link, Button, Skeleton, BN } from 'src/common';
-import { URLS } from 'src/router/urls';
+import { Typography, Button, Skeleton, BN, Plate } from 'src/common';
 import { SablecoinInfo } from '../use-stable-coin-info';
 import { useStablecoinEllipseStyles } from './stablecoin-ellipse.styles';
 
@@ -30,51 +29,42 @@ export const StablecoinEllipse: React.FC<StablecoinEllipseProps> = (props) => {
   );
 
   return (
-    <div className={props.className}>
-      <div className={classes.ellipse}>
-        <Typography variant="h1" align="center" className={classes.title}>
-          Decentralized stablecoin based on real-world assets
+    <div className={clsx(classes.root, props.className)}>
+      <Plate>
+        <Typography variant="body1" align="center">
+          <Typography variant="inherit" component="span">
+            {props.loading && <Skeleton className={classes.skeleton} />}
+            {!props.loading && (
+              <>
+                Total Issued today:{' '}
+                <Typography variant="inherit" component="span" weight="bold">
+                  {totalLiquidityUSD} USDap
+                </Typography>
+              </>
+            )}
+          </Typography>
         </Typography>
-        <div>
-          <Typography variant="body1" align="center" className={classes.info}>
-            <Typography
-              variant="inherit"
-              component="span"
-              className={classes.supply}
-            >
-              {props.loading && <Skeleton className={classes.skeleton} />}
-              {!props.loading && (
-                <>
-                  Total Issued today:{' '}
-                  <Typography variant="inherit" component="span" weight="bold">
-                    {totalLiquidityUSD} USDap
-                  </Typography>
-                </>
-              )}
+      </Plate>
+      <Plate className={classes.sellingBuying}>
+        <div className={classes.info}>
+          <Typography variant="body1" align="center">
+            Circulating Supply:{' '}
+            <Typography variant="inherit" weight="bold">
+              64,840,720 USDap
             </Typography>
           </Typography>
-          <div className={classes.actions}>
-            <Button onClick={props.onBuy}>Buy</Button>
-            <Button onClick={props.onSell}>Sell</Button>
-          </div>
-        </div>
-      </div>
-      <div className={classes.subtext}>
-        <Typography variant="h4" align="center">
-          <Typography variant="inherit" component="p" align="center">
-            USDap is the first-ever decentralized stablecoin based on a basket
-            of real-world debt obligations. The price of USDap equals $1 at all
-            times and the asset is issued only with sufficient collateral.
+          <Typography variant="body1" align="center">
+            Volume (24h):{' '}
+            <Typography variant="inherit" weight="bold">
+              124,720 USDap
+            </Typography>
           </Typography>
-          <Link
-            color="blue"
-            component={ReactRouterLink}
-            to={`${URLS.whitepaper}#3`}
-          >
-            Learn more
-          </Link>
-        </Typography>
-      </div>
+        </div>
+        <div className={classes.actions}>
+          <Button onClick={props.onBuy}>Buy</Button>
+          <Button onClick={props.onSell}>Sell</Button>
+        </div>
+      </Plate>
     </div>
   );
 };
