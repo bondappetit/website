d133048b6a3324723f4f4645d5d7610128e20448
lorof <gilyazov@list.ru>
2021-05-13 16:53:12 +0500

    fix

--- src/bridge/common/contracts/bbag-contract.ts
+++ src/bridge/common/contracts/bbag-contract.ts
@@ -1,11 +1,12 @@
 import { useMemo } from 'react';
 import type { AbiItem } from 'web3-utils';
+import networks from '@bondappetit/networks';
 
 import { useLibrary } from 'src/common';
 import type { BbagAbi } from '../types/bbag-abi';
 import abi from '../abi/bbag-abi.json';
 
-const ADDRESS = '0x1AD0132D8B5Ef3cEBDA1A9692f36AC30be871b6b';
+const ADDRESS = networks.mainBSC.assets.bBAG.address;
 
 export const useBBagContract = () => {
   const library = useLibrary(true);
