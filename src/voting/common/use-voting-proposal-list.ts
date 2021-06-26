efedf840bde629acbd71339952d8c369541210bb
lorof <gilyazov@list.ru>
2021-06-26 20:33:13 +0500

    hide failed proposals

--- src/voting/common/use-voting-proposal-list.ts
+++ src/voting/common/use-voting-proposal-list.ts
@@ -8,6 +8,8 @@ import { useVotingEvents } from './use-voting-events';
 import { usePagination } from './use-pagination';
 import { getProposal } from './get-proposal';
 
+const FAILED_PROPOSALS = [15, 13, 10, 7];
+
 export const useVotingProposalList = (limit?: number) => {
   const { nextPage, getPages, pages, currentPage } = usePagination(limit);
   const governorContract = useGovernorContract();
@@ -20,15 +22,27 @@ export const useVotingProposalList = (limit?: number) => {
 
     const proposalCount = await governorContract.methods.proposalCount().call();
 
-    const proposalPages = getPages(Number(proposalCount));
+    const isMainnet = chainId === 1;
 
-    const existingProposals = proposalPages.map((proposalId) => {
+    const proposalPages = getPages(
+      isMainnet
+        ? Number(proposalCount) - FAILED_PROPOSALS.length
+        : Number(proposalCount)
+    );
+
+    const allProposals = proposalPages.map((proposalId) => {
       return getProposal(proposalId)(governorContract)(eventData)(
         networkConfig
       );
     });
 
-    return Promise.all(existingProposals);
+    if (isMainnet) {
+      return (await Promise.all(allProposals)).filter(
+        ({ id }) => !FAILED_PROPOSALS.includes(Number(id))
+      );
+    }
+
+    return Promise.all(allProposals);
   }, [governorContract, eventData, networkConfig, chainId, getPages]);
 
   return useMemo(
